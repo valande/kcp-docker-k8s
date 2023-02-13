85e494154cdbc2469872f050963ecd831edaebad
Valande <valande@gmail.com>
2023-02-13 23:36:29 +0100

    Add json output logging

--- app/valapp.py
+++ app/valapp.py
@@ -1,10 +1,23 @@
-import time
+import datetime
+import logging
+import sys
+import json_logging
 import redis
 
 from flask import Flask
 
-app = Flask(__name__)
+
 cache = redis.Redis(host='redis', port=6379)
+app = Flask(__name__)
+
+json_logging.init_flask(enable_json=True)
+json_logging.init_request_instrument(app)
+
+# init the logger as usual
+logger = logging.getLogger("valogger")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
 def get_hit_count():
     retries = 5
     while True:
@@ -16,7 +29,25 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+def get_correl():
+    return json_logging.get_correlation_id()
+
 @app.route('/')
 def hello():
+    logger.info("test log statement")
+    correlation_id = json_logging.get_correlation_id()
     count = get_hit_count()
     return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+
+@app.route('/correl')
+def correl():
+    logger.info("correl")
+    correl_id = get_correl()
+    return 'json logger correlation id: {}\n'.format(correl_id)
+
+@app.route('/forcerr')
+def forcerr():
+    logger.error("forcerr")
+    logger.error("forcerr with extra props", extra={'props': {"extra_property": 'extra_value'}})
+    correl_id = get_correl()
+    return 'json logger correlation id: {}\n'.format(correl_id)
